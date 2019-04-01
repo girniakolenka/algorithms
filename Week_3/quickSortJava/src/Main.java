bfd6eb4d24de17529713b206a59402a0fbeefc64
girniakolenka <olenkagirniak@Olenas-MacBook-Pro.local>
2019-04-02 00:15:51 +0300

    Implement task with medium element

--- Week_3/quickSortJava/src/Main.java
+++ Week_3/quickSortJava/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.List;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -11,15 +12,15 @@ public class Main {
     public static void main(String[] args) {
         try {
             Charset charset = Charset.forName("ISO-8859-1");
-            String fileName = "/Users/olenkagirniak/Development/Prometheus/algorithms/Week_3/data_examples_03/input__10000.txt";
+            String fileName = "/Users/olenkagirniak/Development/Prometheus/algorithms/Week_3/data_examples_03/input__1000.txt";
             List<String> allLines = Files.readAllLines(Paths.get(fileName), charset);
 
             allLines.remove(0);
             quickSort(allLines, 0, allLines.size() - 1);
 
-/*            for (String line : allLines) {
+            for (String line : allLines) {
                 System.out.println(line);
-            }*/
+            }
 
             System.out.println(count);
 
@@ -38,6 +39,8 @@ public class Main {
     }
 
     public static Integer getPartition(List<String> arr, Integer start, Integer end) {
+        getPartitionElement(arr, start, end);
+
         String x = arr.get(end);
         Integer i = start - 1;
 
@@ -58,5 +61,39 @@ public class Main {
         arr.set(partition, x);
 
         return partition;
+    }
+
+    public static class Partition implements Comparable{
+        Integer value;
+        Integer index;
+
+        public Partition(Integer value, Integer index) {
+            this.value = value;
+            this.index = index;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            return this.value - ((Partition)o).value;
+        }
+    }
+
+    public static Integer getPartitionElement(List<String> arr, Integer start, Integer end) {
+        String lastElem = arr.get(end);
+        Integer midIndex = Math.abs((start + end)/2);
+        Integer midElement= Integer.valueOf(arr.get(midIndex));
+        Partition[] partitionArr = {
+                new Partition(Integer.valueOf(arr.get(start)), start),
+                new Partition(midElement, midIndex),
+                new Partition(Integer.valueOf(lastElem), end)
+        };
+        
+        Arrays.sort(partitionArr);
+
+        Integer partitionIndex = partitionArr[1].index;
+        arr.set(end, arr.get(partitionIndex));
+        arr.set(partitionIndex, lastElem);
+
+        return partitionArr[1].index;
     }
 }
