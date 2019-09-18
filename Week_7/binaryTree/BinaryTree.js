4d04447e21e39e965067c20fbc462bc5cd57c747
girniakolenka <olenkagirniak@Olenas-MacBook-Pro.local>
2019-09-18 18:47:48 +0300

    Refactor binary tree

--- Week_7/binaryTree/BinaryTree.js
+++ Week_7/binaryTree/BinaryTree.js
@@ -1,72 +1,71 @@
 class BinaryTree {
     constructor(arr) {
         const data = arr[0].split(" ");
-        this._tree = {};
-        this._printInOrder = [];
-        this._count = 0;
 
-        this.createTreeFromPreOrder(data, 0, null);
-        this.createBinaryTree(this._tree);
+        console.log(this.createBinaryTree(data));
     }
 
-    createTreeFromPreOrder(arr, index, previousNode) {
+    createTreeFromPreOrder(arr, tree, index, previousNode) {
         const value = Number(arr[index]);
         const node = new Node(value, previousNode);
 
         if(index === arr.length) {
-            return;
+            return tree;
         }
 
-        if(Object.keys(this._tree).length === 0) {
-            this._tree = node;
-            return this.createTreeFromPreOrder(arr, ++index, node);
+        if(Object.keys(tree).length === 0) {
+            tree = node;
+            return this.createTreeFromPreOrder(arr, tree, ++index, node);
         }
 
         if(previousNode.left === null) {
             previousNode.left = node;
-            return this.createTreeFromPreOrder(arr, ++index, node.data === 0 ? node.parent : node);
+            return this.createTreeFromPreOrder(arr, tree, ++index, node.data === 0 ? node.parent : node);
         }
 
         if(previousNode.left !== null && previousNode.right === null) {
             previousNode.right = node;
 
-            return this.createTreeFromPreOrder(arr, ++index, node.data === 0 ? node.parent : node);
+            return this.createTreeFromPreOrder(arr, tree, ++index, node.data === 0 ? node.parent : node);
         }
 
         if(previousNode.left !== null && previousNode.right !== null) {
-            return this.createTreeFromPreOrder(arr, index, previousNode.parent);
+            return this.createTreeFromPreOrder(arr, tree, index, previousNode.parent);
         }
     }
 
-    printInOrder(node) {
+    createInOrderArr(node, inOrderArr) {
         if(node !== null) {
-            this.printInOrder(node.left);
+            this.createInOrderArr(node.left, inOrderArr);
             if(node.data !== 0) {
-                this._printInOrder.push(node.data);
+                inOrderArr.push(node.data);
             }
-            this.printInOrder(node.right);
+            this.createInOrderArr(node.right, inOrderArr);
         }
     }
 
     createBinaryTree(data) {
-       this.printInOrder(data);
+       const binaryTree = this.createTreeFromPreOrder(data, {}, 0, null);
+       let inOrderArr = [];
+       let index = {
+           current: 0
+       };
 
-       const sortedInOrder = new Sort(this._printInOrder);
+       this.createInOrderArr(binaryTree, inOrderArr);
+       this.setInOrder(new Sort(inOrderArr), binaryTree, index);
 
-       this.setInOrder(sortedInOrder, this._tree);
-
-       console.log(this._tree);
+       return binaryTree;
 
     }
 
-    setInOrder(arr, node) {
-        if( this._count !== arr.length && node !== null && node.data !== 0) {
-            this.setInOrder(arr, node.left);
+    setInOrder(arr, node, index) {
+        if(index.current !== arr.length && node !== null && node.data !== 0) {
+            this.setInOrder(arr, node.left, index);
 
-            node.data = arr[this._count];
-            this._count++;
+            node.data = arr[index.current];
+            index.current++;
 
-            this.setInOrder(arr, node.right);
+            this.setInOrder(arr, node.right, index);
         }
     }
  }
