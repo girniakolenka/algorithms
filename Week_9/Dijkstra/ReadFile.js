0bd62e776fe102a2235011f2e90af1e7353747b7
girniakolenka <olenkagirniak@Olenas-MacBook-Pro.local>
2019-11-22 15:04:28 +0200

    Initial file system

---
+++ Week_9/Dijkstra/ReadFile.js
@@ -0,0 +1,20 @@
+class ReadFile {
+    constructor(url) {
+        return new Promise((resolve, reject) => {
+           this.readTextFile(url, resolve, reject);
+        });
+    }
+
+     readTextFile(url, resolve, reject) {
+        const rawFile = new XMLHttpRequest();
+
+        rawFile.open("GET", url);
+        rawFile.onload = function () {
+            const allText = rawFile.responseText;
+            const lines = allText.split("\n");
+
+            resolve(lines);
+        }
+        rawFile.send(null);
+    }
+}
